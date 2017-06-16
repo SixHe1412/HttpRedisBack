17da34c5e96f971d575cca0901a828355b81ad97
SixHe1412 <872733570@qq.com>
2017-06-16 09:25:53 +0800

    a little change

--- toTimeStamp.py
+++ toTimeStamp.py
@@ -7,6 +7,6 @@ def transToStamp(iTime):
     timestamp = time.mktime(timeArray)
     base_timeArray = time.strptime("2005-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
     base_timestamp = time.mktime(base_timeArray)
-    t_stamp =  str(int(timestamp - base_timestamp))
+    t_stamp = str(int(timestamp - base_timestamp))
 
     return t_stamp
