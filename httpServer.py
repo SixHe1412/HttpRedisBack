b8a14de89ae1578d86166d420413572ff0d3d047
SixHe1412 <872733570@qq.com>
2017-07-05 21:59:13 +0800

    increase new code

--- httpServer.py
+++ httpServer.py
@@ -3,8 +3,10 @@ import SocketServer
 import urlparse
 import json
 import queryRedis
+from SocketServer import ThreadingMixIn
 import queryRedis2
 
+HOST_IP = ''
 PORT = 9001
 
 class myHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
@@ -15,6 +17,7 @@ class myHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
 
         result_list = json.loads(queryRedis.query(qs))
+        #result_list = queryRedis.query(qs)
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
@@ -28,6 +31,32 @@ class myHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         return
 
 
-httpd = SocketServer.TCPServer(("", PORT), myHandler)
+    #def do_GET(self):
+    #    try:
+    #        print "GET"
+    #        qs = urlparse.parse_qs(urlparse.urlparse(self.path).query)
+#
+#
+    #        result_list = json.loads(queryRedis.query(qs))
+    #        #result_list = queryRedis.query(qs)
+#
+    #        self.send_response(200)
+    #        self.send_header('Content-type', 'application/json')
+    #        self.send_header('Access-Control-Allow-Origin', '*')
+    #        self.end_headers()
+#
+    #        #print result_list
+    #        print len(result_list)
+#
+    #        self.wfile.write(json.dumps(result_list))
+    #    except IOError:
+    #        self.send_error(404, message=None)
+
+#class ThreadingHttpServer( ThreadingMixIn, SimpleHTTPServer ):
+#    pass
+
+
+httpd = SocketServer.TCPServer((HOST_IP, PORT), myHandler)
+#httpd = ThreadingHttpServer((HOST_IP, PORT), myHandler)
 print "serving at port", PORT
 httpd.serve_forever()
